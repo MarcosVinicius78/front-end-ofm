4a77bcd480d10ca3f417892a7cfbd220d37269a5
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-07-22 00:17:04 -0300

    tt

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,12 +1,12 @@
 export const environment = {
   // apiUrl: 'http://62.72.11.56/api'
-  apiUrl: 'http://localhost:8080',
-  site: 2
+  // apiUrl: 'http://localhost:8080',
+  site: 2,
   // apiUrl: 'https://www.systemdevmv.site/api'
   // apiUrl: 'http://www.systemdevmv.site/api'
 
   // esse aqui
-  // apiUrl: 'https://sergipeofertas.com.br/api'
+  apiUrl: 'https://sergipeofertas.com.br/api'
 
 
   // apiUrl: 'http://www.sergipeofertas.com.br/api'

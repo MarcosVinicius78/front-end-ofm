12ed2365ee55128b888680c1ce593b0650d29b46
MarcosVinicius78 <marcosvinicius78bol@gmail.com>
2024-07-24 23:56:28 -0300

    teste

--- server.js
+++ server.js
@@ -1,19 +1,38 @@
 const express = require('express');
-const path = require('path');
+const { join } = require('path');
+const { ngExpressEngine } = require('@nguniversal/express-engine');
+const { APP_BASE_HREF } = require('@angular/common');
+const { provideModuleMap } = require('@nguniversal/module-map-ngfactory-loader');
+const { ModuleMapServerLoader } = require('@nguniversal/module-map-ngfactory-loader');
+const { AppServerModuleNgFactory, LAZY_MODULE_MAP } = require('./dist/server/main.js');
+
 const app = express();
-
 const PORT = process.env.PORT || 4000;
-const DIST_FOLDER = path.join(process.cwd(), 'dist/browser');
-const SERVER_FOLDER = path.join(process.cwd(), 'dist/server');
+const DIST_FOLDER = join(process.cwd(), 'dist/browser');
+const APP_SERVER_MODULE = AppServerModuleNgFactory;
 
-// Serve only the static files form the dist directory
-app.use(express.static(DIST_FOLDER));
+// The Universal engine
+app.engine('html', ngExpressEngine({
+  bootstrap: APP_SERVER_MODULE,
+  providers: [
+    provideModuleMap(LAZY_MODULE_MAP),
+    { provide: APP_BASE_HREF, useValue: '/' }
+  ]
+}));
 
-// Pointing to the server-side rendered file
-app.get('/*', (req, res) => {
-  res.sendFile(path.join(SERVER_FOLDER, 'index.html'));
+app.set('view engine', 'html');
+app.set('views', DIST_FOLDER);
+
+// Serve static files
+app.get('*.*', express.static(DIST_FOLDER, {
+  maxAge: '1y'
+}));
+
+// Universal route handler
+app.get('*', (req, res) => {
+  res.render('index', { req });
 });
 
 app.listen(PORT, () => {
-  console.log(`Server is listening on http://localhost:${PORT}`);
+  console.log(`Node server listening on http://localhost:${PORT}`);
 });
